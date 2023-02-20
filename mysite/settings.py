937ec145baf2327b0abbe215502a29a64c4c8c40
Rafik Saad <rs7983@nyu.edu>
2023-02-20 08:16:40 -0500

    deploying to production

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gej820w0!ny9^wcga9_ou4cm6xe#5q3w$&2fikc70x%30()k1n'
-
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['swe1-app-dev.us-east-1.elasticbeanstalk.com']
 
