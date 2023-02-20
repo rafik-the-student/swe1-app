ed285e85395b43fcf1bcc858154acbf10237f3cd
Rafik Saad <rs7983@nyu.edu>
2023-02-20 08:39:25 -0500

    redirected homepage to polls index

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
+
 urlpatterns = [
+    path('', views.index, name='homepage'),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
